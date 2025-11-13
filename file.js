db4c35cbb4a1c5fe4b06bea9a5ea59986be9b34c
hide card <hidecard1500@gmail.com>
2025-11-13 15:04:44 +0630

    lesson1

--- file.js
+++ file.js
@@ -1,10 +1,14 @@
 var fs = require('fs')
 
 try{
-    const data = fs.readFileSync('lesson.txt','utf-8');
-    console.log(data);
+    // const data = fs.readFileSync('lesson.txt','utf-8');
+    // console.log(data);
+
+    fs.writeFileSync('lesson.txt','Hello edit is here');
+    console.log("Success");
+    
 }
-catch(error){
-    console.log("error in read file".error);
+catch(err){
+    console.log("error in read file",err);
     
 }

f095b4f95036ea795a478f0febe0688d56d59896
hide card <hidecard1500@gmail.com>
2025-11-18 14:59:38 +0630

    day1

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000, (err) => {
-    if (!err) {
-        console.log("server run");
+// app.listen(3000, (err) => {
+//     if (!err) {
+//         console.log("server run");
         
-    } else {
-        console.log("Not Work");
+//     } else {
+//         console.log("Not Work");
         
-    }
+//     }
+// })
+
+app.get('/user:id', (req, res) => {
+    const UserID = req.params.id
+    res.send(`User id is : ${UserID}`)
 })
+app.listen(3000, ()=>{
+    console.log("Server is run");
+})
+
